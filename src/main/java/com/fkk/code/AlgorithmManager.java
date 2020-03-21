b9aae332a7f5098da0cfee287f7e6cc6130b776a
karl <coderfan@qq.com>
2020-03-21 19:32:12 +0800

    add: heap sort & counting sort & bucket sort

--- src/main/java/com/fkk/code/AlgorithmManager.java
+++ src/main/java/com/fkk/code/AlgorithmManager.java
@@ -10,12 +10,18 @@ public class AlgorithmManager {
         SortAlgorithm sortAlgorithm = new SortAlgorithm();
         int[] targetArrays = {1, 77, 45, 31, 21, 34, 88, 22, 5, 6, 2};
         int[] targetArrays02 = {5, 4, 3, 2, 1};
-        printArray("Old Arrays",copyArray(targetArrays));
+        printArray("Old Arrays", copyArray(targetArrays02));
 //        printArray("Bubble Sort ", sortAlgorithm.bubbleSort(copyArray(targetArrays02)));
 //        printArray("Selection Sort ", sortAlgorithm.selectionSort(copyArray(targetArrays02)));
 //        printArray("Insertion Sort", sortAlgorithm.insertionSort(copyArray(targetArrays02)));
 //        printArray("Insertion Sort", sortAlgorithm.mergeSort(copyArray(targetArrays02)));
-        printArray("quick sort",sortAlgorithm.quickSort(copyArray(targetArrays02)));
+//        printArray("quick sort",sortAlgorithm.quickSort(copyArray(targetArrays02)));
+//        printArray("heap sort",sortAlgorithm.heapSort(copyArray(targetArrays02)));
+
+        int[] countingArray = {3, 2, 3, 5, 6, 1, 7, 3, 2, 6, 9};
+        printArray("counting sort", sortAlgorithm.countingSort(countingArray));
+
+        sortAlgorithm.bucketSort(countingArray);
 
     }
 
--- src/main/java/com/fkk/code/algorithm/SortAlgorithm.java
+++ src/main/java/com/fkk/code/algorithm/SortAlgorithm.java
@@ -1,5 +1,9 @@
 package com.fkk.code.algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SortAlgorithm {
 
     /**
@@ -102,7 +106,7 @@ public class SortAlgorithm {
     /**
      * 对左右数组进行合并排序
      */
-    public int[] mergeArrays(int[] left, int[] right) {
+    private int[] mergeArrays(int[] left, int[] right) {
         int[] mergeArrays = new int[left.length + right.length];
         int a = 0, b = 0;
         for (int i = 0; i < mergeArrays.length; i++) {
@@ -127,12 +131,18 @@ public class SortAlgorithm {
 
 
     /**
-     * 13,15,6,2,21,7,9,18,11,44
+     * 快速排序：
+     * 发起
      */
     public int[] quickSort(int[] arrays) {
         return quickSort(arrays, 0, arrays.length - 1);
     }
 
+    /**
+     * 快速排序：
+     * 1. 选中基准点
+     * 2. 对左右数组进行递归快排
+     */
     private int[] quickSort(int[] arrays, int left, int right) {
         if (left < right) {
             //标定基准，同时以基准对arrays left-right进行快速排序
@@ -146,6 +156,9 @@ public class SortAlgorithm {
         return arrays;
     }
 
+    /**
+     * 快速排序-选择基准元素
+     */
     private int partition(int[] arrays, int left, int right) {
         int pivot = arrays[left];//锚定基准点
         int j = right;
@@ -175,11 +188,141 @@ public class SortAlgorithm {
         return i;
     }
 
-    private int[] swap(int[] array, int i, int j) {
+
+    /**
+     * 堆排序
+     */
+    public int[] heapSort(int[] arrays) {
+        int length = arrays.length;
+        //首先构建大顶堆
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            //找到第一个非叶子节点，下到上，右到左。
+            // 可以自行画图找规律 length / 2 - 1
+            adjustHeap(arrays, i, length);
+        }
+        //每次把堆顶元素与末尾元素交换，堆减少一个元素，直到遍历完成
+        for (int i = length - 1; i > 0; i--) {
+            //交换堆顶元素,与末尾元素
+            swap(arrays, 0, i);
+            //重新构建大顶堆
+            adjustHeap(arrays, 0, i);
+        }
+        return arrays;
+    }
+
+    /**
+     * 堆排序核心 - 调整 大顶堆
+     */
+    private void adjustHeap(int[] arrays, int i, int length) {
+        int temp = arrays[i];//临时存储 i 对应元素
+        //每次选取指定节点的左节点进行比较
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
+            //比较左右节点大小，右节点大选择右节点作为比较对象
+            if (k + 1 < length && arrays[k] < arrays[k + 1]) {
+                k++;
+            }
+            //比较和temp元素的大小, 若temp小则被换掉
+            // 因为每次比较的都是temp，k位置元素不用改变，最后再改变即可
+            if (arrays[k] > temp) {
+                arrays[i] = arrays[k];
+                i = k;//i直接用来存储每次交换后新的k的位置
+            }
+
+        }
+        //最终i的值确定以后把临时temp存进去，完成大顶堆的调整
+        arrays[i] = temp;
+    }
+
+    /**
+     * 计数排序
+     * 1. 遍历原数组，找出最大和最小元素
+     * 2. 根据最大和最小元素创建 计数数组C
+     * 3. 遍历原数组，将每一个元素出现的次数存入C中指定位置
+     * 4. 对C中每一项都与前一项进行相加运算
+     * 5. 倒叙遍历原数组，根据C中i项的值确定在原数组中的位置，同时C[i]-1
+     */
+    public int[] countingSort(int[] arrays) {
+        if (arrays == null || arrays.length <= 1) {
+            return arrays;
+        }
+        int min = arrays[0], max = arrays[0];
+        int srcLength = arrays.length;
+        //遍历找到最大值和最小值
+        for (int temp : arrays) {
+            max = Math.max(temp, max);
+            min = Math.min(temp, min);
+        }
+
+        //构建计数数组
+        int bucketLength = max - min + 1;
+        int[] bucketArray = new int[bucketLength];
+
+        //再次遍历原数组，找出找出元素重复数量
+        for (int array : arrays) {
+            bucketArray[array - min]++;
+        }
+
+        //每一项与前一项叠加
+        for (int i = 1; i < bucketLength; i++) {
+            bucketArray[i] += bucketArray[i - 1];
+        }
+
+        //开始再次遍历原数组
+        int[] dstArray = new int[srcLength];
+        for (int i = srcLength - 1; i >= 0; i--) {
+            int target = arrays[i];
+            //计算得到当前元素之前又多少个元素
+            int bucketIndex = target - min;
+            int index = bucketArray[bucketIndex] - 1;
+            dstArray[index] = target;
+            bucketArray[bucketIndex]--;
+        }
+        return dstArray;
+    }
+
+    /**
+     * 桶排序：
+     * 1. 计数排序是特殊的桶排序
+     * 2. 桶排序也需要开辟内存空间
+     * 3. 内存空间由序列最大值决定
+     */
+    public void bucketSort(int[] arrays) {
+        int min = arrays[0], max = arrays[0];
+        int srcLength = arrays.length;
+        //遍历找到最大值和最小值
+        for (int temp : arrays) {
+            max = Math.max(temp, max);
+            min = Math.min(temp, min);
+        }
+        //也可以使用别的分组方式
+        int bucketSize = (max - min) / srcLength + 1;
+
+        //构建桶
+        List<ArrayList<Integer>> bucketList = new ArrayList<>();
+        for (int i = 0; i < bucketSize; i++) {
+            bucketList.add(new ArrayList<>());
+        }
+
+        for (int array : arrays) {
+            int index = (array - min) / srcLength;
+            bucketList.get(index).add(array);
+        }
+
+        //对桶中元素进行排序
+        for (ArrayList<Integer> item : bucketList) {
+            if (!item.isEmpty()){
+                //使用官方API，也可以根据需要使用别的排序法
+                Collections.sort(item);
+            }
+        }
+
+        System.out.println("bucket arrays: " + bucketList.toString());
+    }
+
+    private void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-        return array;
     }
 
 }
