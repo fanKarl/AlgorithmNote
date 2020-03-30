6aaed850dc2a970551326903bb027caf0ed49df9
karl <coderfan@qq.com>
2020-03-30 23:05:22 +0800

    fix:无重复字符串滑动窗口解法

--- src/main/java/com/fkk/code/leetcode/LengthOfLongestSubstring.java
+++ src/main/java/com/fkk/code/leetcode/LengthOfLongestSubstring.java
@@ -2,11 +2,35 @@ package com.fkk.code.leetcode;
 
 import com.fkk.code.utils.Log;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 无重复字符的最长子串：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  */
 public class LengthOfLongestSubstring {
 
+    public int solutionB(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)) {
+                start = Math.max(map.get(ch) + 1, start);
+            }
+
+            max = Math.max(max, end - start + 1);
+            map.put(ch, end);
+        }
+
+        return max;
+    }
 
 
     public int solution(String s) {
